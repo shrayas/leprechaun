2c73ba6aced720321ebc021ae2e7e354ac7f95c2
Zach Dziura <zcdziura@gmail.com>
2013-05-19 17:52:05 -0400

    Refactoring into a Python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name='Leprechaun',
+  version="0.1.0",
+  author="Zach Dziura",
+  author_email="zcdziura@gmail.com",
+  packages=["leprechaun"],
+  url="https://github.com/zcdziura/leprechaun",
+  license="LICENSE.txt",
+  description="A simple rainbow table generator",
+  long_description=open("README.md").read()
+)
